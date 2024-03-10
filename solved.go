d9a483bcb8c69dc4d0b1f8fa8a1fb7a032be17b4
Abiria <abiria.dev@gmail.com>
2024-03-10 19:04:41 +0900

    feat: define `User` type

--- solved.go
+++ solved.go
@@ -1,8 +1,24 @@
 package main
 
+type User struct {
+  handle string
+  bio string
+  solvedCount int
+  voteCount int
+  tier int
+  rating int
+  class int
+  classDecoration string
+  rivalCount int
+  reverseRivalCount int
+  rank int
+}
+
 func fetchUserData(user string) (User, error) {}
   res, err := http.Get("https://solved.ac/api/v3/user/show?handle=" + user)
   if err != nil {
-  return User{}, err
+    return User{}, err
   }
+
+  return User{}, nil
 }
