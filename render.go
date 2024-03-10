66ce0630b98dc37fb5b8f98849e3c363d5194e34
Abiria <abiria.dev@gmail.com>
2024-03-10 19:58:31 +0900

    refactor: make `render` a method

--- render.go
+++ render.go
@@ -16,11 +16,11 @@ type GistProp struct {
 	Rank      string
 }
 
-func renderGist(user User) (string, error) {
+func (prop GistProp) Render() (string, error) {
 	var buf strings.Builder
 	if tmpl, err := template.New("gist").ParseFiles(gistTemplate); err != nil {
 		return "", err
-	} else if err := tmpl.Execute(&buf, user); err != nil {
+	} else if err := tmpl.Execute(&buf, prop); err != nil {
 		return "", err
 	}
 	return buf.String(), nil
