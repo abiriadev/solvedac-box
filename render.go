1cb369dee3e4d6f9fda7ccbc58caf56a789756c3
Abiria <abiria.dev@gmail.com>
2024-03-11 10:32:43 +0900

    feat: use emoji mapper

--- render.go
+++ render.go
@@ -11,20 +11,25 @@ type GistProp struct {
 	Handle    string
 	Bio       string
 	Tier      string
-	TierEmoji string
+	TierEmoji rune
 	Rating    string
 	Rank      string
 }
 
-func NewGistPropFromUser(user User) GistProp {
+func NewGistPropFromUser(user User) (GistProp, error) {
+	emoji, err := TierToEmoji(user.Tier)
+	if err != nil {
+		return GistProp{}, err
+	}
+
 	return GistProp{
 		Handle:    user.Handle,
 		Bio:       user.Bio,
 		Tier:      string(user.Tier),
-		TierEmoji: string(user.Tier),
+		TierEmoji: emoji,
 		Rating:    string(user.Rating),
 		Rank:      string(user.Rank),
-	}
+	}, nil
 }
 
 func (prop GistProp) Render() (string, error) {
