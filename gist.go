f5f0edc65dc461622044596ee50d20451e2d59cd
Abiria <abiria.dev@gmail.com>
2024-03-11 15:51:50 +0900

    feat: implement method

--- gist.go
+++ gist.go
@@ -1,11 +1,28 @@
 package main
 
-import "github.com/google/go-github/v60/github"
+import (
+	"context"
+
+	"github.com/google/go-github/v60/github"
+)
 
 type GistClient struct {
 	client github.Client
 }
 
-func (client GistClient) UpdateGist(id, content string) {
-	// todo!()
+func (client GistClient) UpdateGist(id, filename, content string) error {
+	ctx := context.Background()
+
+	gist, _, err := client.client.Gists.Get(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	gFilename := github.GistFilename(filename)
+	f := gist.Files[gFilename]
+	f.Content = &content
+	gist.Files[gFilename] = f
+
+	_, _, err = client.client.Gists.Edit(ctx, id, gist)
+	return err
 }
